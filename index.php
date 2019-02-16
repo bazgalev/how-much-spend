7370ff2d4a4c300f080be2eabc5def535bf3be34
bazgalev <pazgalevm@gmail>
2019-02-16 14:23:17 +0300

    ver 1.0

--- index.php
+++ index.php
@@ -13,7 +13,7 @@ ini_set('display_startup_errors', 1);
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Транспорт</title>
+    <title>Затраты на транспорт</title>
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 </head>
 <body>
@@ -45,20 +45,23 @@ ini_set('display_startup_errors', 1);
 
 <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
 <script>
-    $("#getSum").click(function () {
-        var subway = $("#subwayInput").val();
-        var ground = $("#groundInput").val();
+    $.(function () {
+        $("#getSum").click(function () {
+            var subway = $("#subwayInput").val();
+            var ground = $("#groundInput").val();
 
-        $.ajax({
-            method: "POST",
-            url: "ajaxHandler.php",
-            data: {subway: subway, ground: ground},
-        })
-            .done(function (response) {
-                $("form").html(response);
+            $.ajax({
+                method: "POST",
+                url: "ajaxHandler.php",
+                data: {subway: subway, ground: ground},
+            })
+                .done(function (response) {
+                    $("form").html(response);
 
-            });
+                });
+        });
     });
+
 </script>
 </body>
 </html>
